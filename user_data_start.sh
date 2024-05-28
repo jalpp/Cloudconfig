6b73b0e557eb69f29be64e0690e51eb516e66181
jalpp <92553013+jalpp@users.noreply.github.com>
2024-05-28 16:54:06 -0400

    Update user_data_start.sh

--- user_data_start.sh
+++ user_data_start.sh
@@ -4,7 +4,6 @@ yum update -y
 yum install -y httpd
 systemctl start httpd
 systemctl enable httpd
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install 18.12.1
+sudo amazon-linux-extras install epel
+sudo yum install nodejs
 echo "<div> <h1>Server Private IP $(hostname -f)</h1> </div> <div> <p> This is a test server user data script which is created from command line start up and installing Node js </p> </div>" > /var/www/html/index.html
