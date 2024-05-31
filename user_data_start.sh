6713bcf0c983a3fa4b978c23a47e643619a03443
jalpp <92553013+jalpp@users.noreply.github.com>
2024-05-31 17:34:15 -0400

    Update user_data_start.sh

--- user_data_start.sh
+++ user_data_start.sh
@@ -4,6 +4,5 @@ yum update -y
 yum install -y httpd
 systemctl start httpd
 systemctl enable httpd
-sudo amazon-linux-extras install epel
 sudo yum install nodejs
 echo "<div> <h1>Server Private IP $(hostname -f)</h1> </div> <div> <p> This is a test server user data script which is created from command line start up and installing Node js </p> </div>" > /var/www/html/index.html
